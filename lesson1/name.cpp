09c43bbe220e8a6068df3181ac134b59b23255f7
Artem <7phoenix.rainbow7@gmail.com>
2019-10-06 09:58:25 +0000

    first commit

---
+++ lesson1/name.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// #include "iostream"
+// #include "iostream.h"
+// #include <stdlib.h> ""
+// #include <cstdlib>
+// #include <stding.h>
+
+
+
+int main(){
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
